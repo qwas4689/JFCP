b0745a7d913c126a0399bb21497ff315928314f6
Klistas <92723493+Klistas@users.noreply.github.com>
2022-10-05 08:58:55 +0900

    [Feat] Inventory & Interaction 구현 (#4)
    - 기본 메서드 토대 구현
    - 세부 사항은 Item과 연결해야함.

---
+++ Assets/Scripts/Player/PlayerInventory.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInventory : MonoBehaviour
+{
+    public List<Item> Inventory = new List<Item>();
+
+    private void Start()
+    {
+        
+    }
+
+    public void PushItemToInventory(Item item)
+    {
+        if(item != null)
+        { 
+            Inventory.Add(item);
+        }
+    }
+
+    public Item PopItemFromInventory(Item item)
+    {
+        if(Inventory.Contains(item))
+        {
+            Inventory.Remove(item);
+            return item;
+
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    public void StoreItem(Item item)
+    {
+        // item.GetComponent<Item>().SetStored();
+    }
+}
