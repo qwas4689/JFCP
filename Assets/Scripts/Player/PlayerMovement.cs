0beab3fcedb6629e91b056561e00a257a788b7d7
Klistas <92723493+Klistas@users.noreply.github.com>
2022-10-04 12:21:33 +0900

    Player movement king (#3)
    * [Feat] 기본 Movement 구현_King
    
    - Move()와 Rotate() 구현
    - Move는 Rigidbody의 MovePosition, Rotate는 CameraRig의 Rotation과 동기화.
    
    * [Fix] Z Input에 _moveSpeed 추가
    
    - Z 축 이동에 _moveSpeed를 빼먹었습니다...

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -4,13 +4,34 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField]
+    private Transform _cameraRig;
+    private PlayerInput _playerInput;
+    private Rigidbody _rigidbody;
+    [SerializeField]
+    private float _moveSpeed;
+    private float _rotateSpeed;
     void Start()
     {
-        
+        _playerInput = GetComponent<PlayerInput>();
+        _rigidbody = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
-        
+        Move();
+        Rotate();
     }
+
+    void Move()
+    {
+        _rigidbody.MovePosition(new Vector3(_playerInput.Xinput * _moveSpeed, 0, _playerInput.Zinput * _moveSpeed));
+
+    }
+    void Rotate()
+    {
+        transform.rotation = _cameraRig.rotation;
+    }
+
+
 }
