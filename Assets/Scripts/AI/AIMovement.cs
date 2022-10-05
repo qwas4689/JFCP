9daf400b8fdc023e3bf8591cd3f37d816918278b
qwas4689 <qwas4689@gmail.com>
2022-10-05 17:52:08 +0900

    [Chore] AISensor / AIInteraction
    AISensor 80% 작성완료
    AIInteraction 작성중

--- Assets/Scripts/AI/AIMovement.cs
+++ Assets/Scripts/AI/AIMovement.cs
@@ -1,20 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class AIMovement : MonoBehaviour
 {
+    private float moveSpeed = 0.5f;
+
     private Transform _targetPosition;
 
+    private NavMeshAgent navMeshAgent;
 
     void Start()
     {
-        
+        navMeshAgent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // _targetPosition = 도와줘요 진호에몽
+
+        if (/* AI가 행동을 마칠 때 true*/ true)
+        {
+            MoveToNextItem(_targetPosition);
+        }
     }
+
+    // 
+    private void MoveToNextItem(Transform NextItemPosition)
+    {
+        navMeshAgent.speed = moveSpeed;
+        navMeshAgent.SetDestination(NextItemPosition.position);
+    }
+
 }
