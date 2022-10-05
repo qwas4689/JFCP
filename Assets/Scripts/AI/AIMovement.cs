51640f50e6c5d68e987f2abd3c6c3a87f8378047
strange-tiger <jinolee9525@gmail.com>
2022-10-05 14:12:05 +0900

    [Feat] AIMovement

--- Assets/Scripts/AI/AIMovement.cs
+++ Assets/Scripts/AI/AIMovement.cs
@@ -5,12 +5,30 @@ using UnityEngine.AI;
 
 public class AIMovement : MonoBehaviour
 {
+    private AIInteraction _interaction;
     private NavMeshAgent _agent;
     private Transform _targetPosition;
+    private Item _nextItem;
+    private LayerMask _itemLayer = 1 << 3;
     private void Awake()
     {
+        _interaction = GetComponent<AIInteraction>();
         _agent = GetComponent<NavMeshAgent>();
     }
 
+    private void SetTarget()
+    {
+        //_nextItem = _interaction.GetNextItem();
+        _targetPosition = _nextItem.Data.OriginalPosition;
+    }
 
+    private void FindTarget()
+    {
+        Collider[] candidates = new Collider[5];
+        Physics.OverlapSphereNonAlloc(transform.position, 10f, candidates, _itemLayer);
+        foreach(Collider item in candidates)
+        {
+
+        }
+    }
 }
