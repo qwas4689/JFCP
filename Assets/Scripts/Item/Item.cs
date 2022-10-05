ded0daad6e99c8650c5080f7eb097551d151afb0
strange-tiger <103083761+strange-tiger@users.noreply.github.com>
2022-10-05 09:35:23 +0900

    [Feat] 아이템 구현 (#5)
    * [Feat] 아이템 구현
    
    - 아이템 순서는 Heap으로 구현
    - Item.cs / ItemData.cs / ItemManager
    - Util->Heap.cs
    
    * [Fix] 오류 수정 & 리팩토링
    
    - 인덱스 계산에 -1을 잊은 문제 해결
    - Pop과 Remove 결과 반환 구현
    - 코드 리팩토링

---
+++ Assets/Scripts/Item/Item.cs
@@ -0,0 +1,105 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum EItemState
+{
+    UNPACKED,
+    PACKED,
+    GRASPED,
+    STORED,
+    MAX
+}
+
+public class Item : MonoBehaviour
+{
+    public EItemState CurrentState { get; private set; }
+    public ItemData Data;
+
+    //private ItemManager _manager;
+    private Rigidbody _rigidbody;
+    private OVRGrabbable _grabbable;
+    private bool _isPacked;
+    
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        _grabbable = GetComponent<OVRGrabbable>();
+        CurrentState = EItemState.UNPACKED;
+        _isPacked = false;
+
+        //ItemManager.Instance.Items.Push(this);
+    }
+
+    // Heap 테스트 용
+    //private void Start()
+    //{
+    //    int[] ints = { 4, 3, 2, 5, 1 };
+
+    //    Heap.Heap<int> heap = new Heap.Heap<int>(compare);
+
+    //    foreach(int num in ints)
+    //    {
+    //        heap.Push(num);
+    //    }
+
+    //    while(!heap.Empty())
+    //    {
+    //        Debug.Log(heap.Pop());
+    //    }
+    //}
+
+    private bool compare(int left, int right)
+    {
+        return left >= right;
+    }
+
+    private void Update()
+    {
+        if(_grabbable.isGrabbed)
+        {
+            SetGrasped();
+        }
+        else
+        {
+            if (_isPacked)
+            {
+                CurrentState = EItemState.PACKED;
+            }
+            else
+            {
+                CurrentState = EItemState.UNPACKED;
+            }
+        }
+    }
+
+    public void SetPacked()
+    {
+        _isPacked = true;
+
+        if (CurrentState == EItemState.UNPACKED)
+        {
+            CurrentState = EItemState.PACKED;
+        }
+    }
+
+    public void SetUnPacked()
+    {
+        _isPacked = false;
+
+        if (CurrentState == EItemState.PACKED)
+        {
+            CurrentState = EItemState.UNPACKED;
+        }
+    }
+
+    public void SetGrasped()
+    {
+        CurrentState = EItemState.GRASPED;
+    }
+
+    public void SetStored()
+    {
+        CurrentState = EItemState.STORED;
+    }
+}
---
+++ Assets/Scripts/Util/Heap.cs
@@ -0,0 +1,286 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+
+namespace Heap
+{
+    public class ItemHeap
+    {
+        private List<Item> _list = new List<Item>();
+        public Item this[int index]
+        {
+            get
+            {
+                return _list[index];
+            }
+
+            //set
+            //{
+            //    _list[index] = value;
+            //}
+        }
+
+        private void Swap(ref int left, int right)
+        {
+            Item temp;
+
+            temp = _list[left - 1];
+            _list[left - 1] = _list[right - 1];
+            _list[right - 1] = temp;
+
+            left = right;
+        }
+
+        public Item Top()
+        {
+            return _list[0];
+        }
+
+        public bool Empty()
+        {
+            return _list.Count == 0;
+        }
+
+        public int Size()
+        {
+            return _list.Count;
+        }
+
+        public void Push(Item item)
+        {
+            _list.Add(item);
+
+            if (item.Data.IsTarget)
+            {
+                return;
+            }
+
+            int currentIndex = Size();
+
+            while (currentIndex > 1)
+            {
+                int parentIndex = currentIndex / 2;
+
+                if (_list[parentIndex - 1].Data.IsTarget)
+                {
+                    Swap(ref currentIndex, parentIndex);
+                    continue;
+                }
+
+                if (_list[parentIndex - 1].Data.Size < _list[currentIndex - 1].Data.Size)
+                {
+                    break;
+                }
+                
+                if (_list[parentIndex - 1].Data.Weight >= _list[currentIndex - 1].Data.Weight)
+                {
+                    break;
+                }
+
+                Swap(ref currentIndex, parentIndex);
+            }
+        }
+
+        public Item Pop()
+        {
+            return Remove(0);
+        }
+
+        public Item Remove(int index)
+        {
+            Item result = _list[index];
+            
+            _list[index] = _list[_list.Count - 1];
+
+            _list.RemoveAt(_list.Count - 1);
+
+            int currentSize = _list.Count;
+            int currentIndex = index + 1;
+
+            while (currentIndex < currentSize)
+            {
+                int left = currentIndex * 2;
+                int right = left + 1;
+
+                if (left > currentSize)
+                {
+                    break;
+                }
+
+                int child = left;
+                if (right <= currentSize)
+                {
+                    if (_list[right - 1].Data.IsTarget)
+                    {
+                        child = left;
+                    }
+                    else if (_list[left - 1].Data.Size > _list[right - 1].Data.Size)
+                    {
+                        child = right;
+                    }
+                    else if (_list[left - 1].Data.Size == _list[right - 1].Data.Size && _list[left - 1].Data.Weight < _list[right - 1].Data.Weight)
+                    {
+                        child = right;
+                    }
+                }
+
+                if (_list[currentIndex - 1].Data.IsTarget)
+                {
+                    Swap(ref currentIndex, child);
+                    continue;
+                }
+
+                if (_list[child - 1].Data.IsTarget)
+                {
+                    break;
+                }
+                
+                if (_list[currentIndex - 1].Data.Size < _list[child - 1].Data.Size)
+                {
+                    break;
+                }
+                
+                if (_list[currentIndex - 1].Data.Size == _list[child - 1].Data.Size && _list[currentIndex - 1].Data.Weight > _list[child - 1].Data.Weight)
+                {
+                    break;
+                }
+
+                Swap(ref currentIndex, child);
+            }
+
+            return result;
+        }
+
+        public void Remove(Item item)
+        {
+            ItemSearch _is = new ItemSearch(item);
+            Remove(_list.FindIndex(_is.EqualsTo));
+        }
+
+        private class ItemSearch
+        {
+            Item _item;
+
+            public ItemSearch(Item item)
+            {
+                _item = item;
+            }
+
+            public bool EqualsTo(Item item)
+            {
+                return _item == item;
+            }
+        }
+    }
+
+    public class Heap<T>
+    {
+        private List<T> _list = new List<T>();
+        public delegate bool Compare<T1, T2>(T1 left, T2 right);
+
+        private Compare<T, T> _compare;
+
+        public Heap (Compare<T, T> compare)
+        {
+            _compare = compare;
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                return _list[index];
+            }
+
+            //set
+            //{
+            //    _list[index] = value;
+            //}
+        }
+
+        private void Swap(ref int left, int right)
+        {
+            T temp;
+
+            temp = _list[left - 1];
+            _list[left - 1] = _list[right - 1];
+            _list[right - 1] = temp;
+
+            left = right;
+        }
+
+        public T Top()
+        {
+            return _list[0];
+        }
+
+        public bool Empty()
+        {
+            return _list.Count == 0;
+        }
+
+        public int Size()
+        {
+            return _list.Count;
+        }
+
+        public void Push(T obj)
+        {
+            _list.Add(obj);
+
+            int currentIndex = Size();
+
+            while (currentIndex > 1)
+            {
+                int parentIndex = currentIndex / 2;
+
+                if (_compare(_list[parentIndex - 1], _list[currentIndex - 1]))
+                {
+                    break;
+                }
+
+                Swap(ref currentIndex, parentIndex);
+            }
+        }
+
+        public T Pop()
+        {
+            T result = _list[0];
+            _list[0] = _list[_list.Count - 1];
+
+            _list.RemoveAt(_list.Count - 1);
+
+            int currentSize = _list.Count;
+            int currentIndex = 1;
+
+            while (currentIndex < currentSize)
+            {
+                int left = currentIndex * 2;
+                int right = left + 1;
+
+                if (left > currentSize)
+                {
+                    break;
+                }
+
+                int child = left;
+                if (right <= currentSize && _compare(_list[right - 1], _list[left - 1]))
+                {
+                    child = right;
+                }
+
+                if (_compare(_list[currentIndex - 1], _list[child - 1]))
+                {
+                    break;
+                }
+                
+                Swap(ref currentIndex, child);
+            }
+
+            return result;
+        }
+    }
+}
