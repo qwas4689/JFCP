a7a9bb0d61e5d6fd14235ec422a80a38c2636514
strange-tiger <jinolee9525@gmail.com>
2022-10-06 14:58:16 +0900

    [Feat] Util - GlobalInstance
    - SingletonBehaviour 추가
    - SingletonBehaviour에서 DontDestroyOnLoad를 제외하여 컴포넌트를 전역 인스턴스화하는 기능만을 구현
    - GlobalInstance를 상속 받는 것으로 사용 가능

--- Assets/Scripts/Item/ItemManager.cs
+++ Assets/Scripts/Item/ItemManager.cs
@@ -3,43 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using DataStructure.Heap;
 
-public class ItemManager : MonoBehaviour
+public class ItemManager : GlobalInstance<ItemManager>
 {
-    private static ItemManager _instance;
-
-    public static ItemManager Instance
-    {
-        get
-        {
-            if (_instance == null)
-            {
-                _instance = FindObjectOfType<ItemManager>();
-            }
-
-            return _instance;
-        }
-    }
-
-    private void Awake()
-    {
-        if (_instance != null)
-        {
-            if(_instance != this)
-            {
-                Destroy(gameObject);
-            }
-
-            return;
-        }
-        _instance = GetComponent<ItemManager>();
-    }
-
-
     public ItemHeap Items = new ItemHeap();
     // IsTarget == true면 가장 마지막
     // 사이즈 우선 비교 -> 작으면 우선 (enum 수치 상 작을 수록 크다)
     // 같다면 무게 비교 -> 무거울 수록 우선
-    
 
     private void SetOrder(Item item)
     {
